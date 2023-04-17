80be16672797058b4a37942b7bfcef11f0081962
Benefrancis <benefrancis@gmail.com>
2023-04-17 08:46:38 -0300

    Criei a Unidade Orgânica para representar as lojas da rede Benezinho Supermercados.

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -4,23 +4,53 @@ import br.com.fiap.model.Categoria;
 import br.com.fiap.model.Cliente;
 import br.com.fiap.model.Pedido;
 import br.com.fiap.model.Produto;
+import br.com.fiap.unidade.model.Unidade;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("maria-db");
         EntityManager manager = factory.createEntityManager();
 
         salvar(manager);
-
         var pedido = findPedidoByid(1L, manager);
-
         System.out.println(pedido);
+
+
+        Unidade sede = new Unidade();
+        sede.setSede(null);
+        sede.setNome("Unidade Brasil");
+
+        Unidade filialSP = new Unidade();
+        filialSP.setSede(sede);
+        filialSP.setNome("São Paulo");
+
+        Unidade filialVilaMariana = new Unidade();
+        filialVilaMariana.setNome("Vila Mariana");
+        filialVilaMariana.setSede(filialSP);
+
+        Unidade filialVilaSantana = new Unidade();
+        filialVilaSantana.setNome("Santana");
+        filialVilaSantana.setSede(filialSP);
+
+
+        Unidade filialVilaSantoAmaro = new Unidade();
+        filialVilaSantoAmaro.setNome("Santo Amaro");
+        filialVilaSantoAmaro.setSede(filialSP);
+
+
+        manager.getTransaction().begin();
+        Arrays.asList(sede, filialSP, filialVilaMariana, filialVilaSantana, filialVilaSantoAmaro).forEach(unidade -> {
+            manager.persist(unidade);
+        });
+        manager.getTransaction().commit();
 
     }
 
---
+++ src/main/java/br/com/fiap/unidade/model/Unidade.java
@@ -0,0 +1,28 @@
+package br.com.fiap.unidade.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_UNIDADE")
+public class Unidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_UNIDADE")
+    @SequenceGenerator(name = "SQ_UNIDADE", sequenceName = "SQ_UNIDADE", initialValue = 1)
+    @Column(name = "ID_UNIDADE")
+    private Long id;
+
+    @Column(name = "NM_UNIDADE")
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "ID_UNIDADE_SEDE",
+            referencedColumnName = "ID_UNIDADE",
+            foreignKey = @ForeignKey(name = "FK_UNIDADE_SEDE", value = ConstraintMode.CONSTRAINT)
+    )
+    private Unidade sede;
+
+}
